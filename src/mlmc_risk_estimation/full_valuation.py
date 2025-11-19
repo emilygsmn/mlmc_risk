887e87ef1e669d757476080ff5dedd9b556dc64e
emilygsmn <emily.gasmann@gmail.com>
2025-11-19 10:53:45 +0100

    add pricing functions (bond, fx, eq)

--- src/mlmc_risk_estimation/full_valuation.py
+++ src/mlmc_risk_estimation/full_valuation.py
@@ -1,6 +1,27 @@
 """"Module providing functions for full valuation of the risk factors."""
 
+import numpy as np
+
 __all__ = ["comp_prices_with_calib_targets"]
+
+def _get_market_price(market_data, instr_name, ref_date):
+    """Function selecting the relevant market price from the historical time series'."""
+    return market_data.loc[market_data["Date"] == ref_date, instr_name].iloc[0]
+
+def _calc_FX_price(market_data, instr_name, ref_date):
+    """Function getting the FX rate quoted in EUR as of the reference date."""
+    return _get_market_price(market_data, instr_name, ref_date)
+
+def _calc_EQ_price(market_data, instr_name, ref_date):
+    """Function getting the equity market price as of the reference date."""
+    return _get_market_price(market_data, instr_name, ref_date)
+
+def _calc_BOND_price(instr_info):
+    """Function pricing a zero-coupon bond excl. inflation and credit risk."""
+    face_val = instr_info["notional_/_pos_units"]
+    rfr = instr_info["rfr"]
+    maturity = instr_info["maturity"]
+    return face_val * np.exp(-rfr * maturity)
 
 def comp_prices_with_calib_targets(base_values, calib_target):
     """Function checking whether the base values are close enough to the calibration targets."""
