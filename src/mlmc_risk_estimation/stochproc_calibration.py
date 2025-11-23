68ffe2c2e446c6b34aded57bf7c5a41961ac74de
emilygsmn <emily.gasmann@gmail.com>
2025-11-23 20:54:40 +0100

    pylint style adjustments

--- src/mlmc_risk_estimation/stochproc_calibration.py
+++ src/mlmc_risk_estimation/stochproc_calibration.py
@@ -7,14 +7,17 @@ import numpy as np
 __all__ = ["calibrate_models"]
 
 def _get_return_time_incr(ret_length):
+    """Function getting the size of a time increment for return calculation."""
+
     if ret_length == "week":
         return 5
     elif ret_length == "quarter":
         return 13
     else:
-        raise ValueError(f"Unknown return length '{ret_length}'. Only weekly and quarterly are implemented.")
+        raise ValueError(f"Unknown return length '{ret_length}'.")
 
 def _calc_rel_returns(time_series, ret_length):
+    """Function calculating the relative returns given a series of prices."""
 
     # Calculate how large the time increments for the return calculation are
     time_incr_size = _get_return_time_incr(ret_length)
@@ -23,10 +26,11 @@ def _calc_rel_returns(time_series, ret_length):
     starts = time_series[0 : len(time_series) - time_incr_size + 1 : time_incr_size]
     ends   = time_series[time_incr_size - 1 : len(time_series) : time_incr_size]
 
-    # Get relative return of every non-overlapping period of days/months 
+    # Get relative return of every non-overlapping period of days/months
     return (ends - starts) / starts
 
 def _calc_abs_returns(time_series, ret_length):
+    """Function calculating the absolute returns given a series of prices."""
 
     # Calculate how large the time increments for the return calculation are
     time_incr_size = _get_return_time_incr(ret_length)
@@ -35,26 +39,32 @@ def _calc_abs_returns(time_series, ret_length):
     starts = time_series[0 : len(time_series) - time_incr_size + 1 : time_incr_size]
     ends   = time_series[time_incr_size - 1 : len(time_series) : time_incr_size]
 
-    # Get absolute return of every non-overlapping period of days/months 
+    # Get absolute return of every non-overlapping period of days/months
     return ends - starts
 
 def _calc_vola_from_time_series(returns, mu=0, dt=4):
+    """Function calculating the empirical volatility from a time series."""
+
     rms_dev = np.sqrt(np.mean((returns - mu)**2))
     t_scaling = 1 / np.sqrt(dt)
     return rms_dev * t_scaling
 
 def _get_num_returns_pa(ret_length):
+    """Function getting the number of returns within a year, given a return period length."""
+
     if ret_length == "week":
         return 52
     elif ret_length == "quarter":
         return 4
     else:
-        ValueError(f"Unknown return length '{ret_length}'. Only weekly and quarterly are implemented.")
+        raise ValueError(f"Unknown return length '{ret_length}'.")
 
 def _convert_to_rel_return_vola(emp_vola):
+    """Fnction converting an absolute return volatility to a relative one."""
     return emp_vola
 
 def _get_empirical_vola(time_series, ret_type="rel", ret_length="quarter", conv_to_rel=False):
+    """Function getting the empirical volatility for a given time series."""
 
     # Calculate the returns by calling the respective return calculation function
     func_name = f"_calc_{ret_type}_returns"
@@ -77,6 +87,7 @@ def _get_empirical_vola(time_series, ret_type="rel", ret_length="quarter", conv_
         return emp_vola
 
 def calibrate_models(mkt_data, instr_info, param_config):
+    """Function calibrating the stochastic processes to given market data."""
 
     calib_methods = param_config["valuation"]["calibr_methods"]
     rfs = mkt_data.columns
@@ -86,10 +97,11 @@ def calibrate_models(mkt_data, instr_info, param_config):
 
     for rf in rfs:
         risk_type = instr_indexed.loc[rf, "val_tag"]
-        volas.loc["sigma", rf] = _get_empirical_vola(time_series=np.array(mkt_data[rf]),
-                                                     ret_type=calib_methods["return_type"][risk_type],
-                                                     ret_length=calib_methods["return_length"][risk_type],
-                                                     conv_to_rel=calib_methods["conv_to_rel"][risk_type]
-                                                     )
+        volas.loc["sigma", rf] = _get_empirical_vola(
+            time_series=np.array(mkt_data[rf]),
+            ret_type=calib_methods["return_type"][risk_type],
+            ret_length=calib_methods["return_length"][risk_type],
+            conv_to_rel=calib_methods["conv_to_rel"][risk_type]
+            )
 
     return volas
